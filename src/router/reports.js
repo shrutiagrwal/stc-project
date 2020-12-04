b46bfb5848b98e691fa3232ad648e3a639f7146e
pawnikhanna <pawnikhanna12@gmail.com>
2020-12-04 21:50:29 +0530

    routes added

--- src/router/reports.js
+++ src/router/reports.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const router = express.Router();
+const Report = require('../models/reports')
 const auth = require('../auth/StudentAuth')
-router.get('/companies', auth, async(req, res) => {
 
+router.get('/reports', auth, async(req, res) => {
+    try{
+        const reports = await Report.find({});
+        res.send('reports');
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
 })
 
+router.get('/report/:id', async(req, res)=> {
+    const report_id = req.params.id;
+    try{
+        const report = await Report.findById(report_id) ;
+        if(!report){
+        return res.status(404).send();
+        }
+        else{
+            res.send(report);
+        }
+    }
+    catch(err){
+        res.status(400).send(err);
+    }
+});
 
-
-
-module.exports = router
+module.exports = router;
