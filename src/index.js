2c7b042a2dca2f5b9358b098812b8260e0990fc3
pawnikhanna <51890749+pawnikhanna@users.noreply.github.com>
2020-12-10 19:27:15 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const mongoDB = require('./db/mongoose')
 const bodyParser = require('body-parser');
 const login = require('./router/login')
 const report = require('./router/reports')
+const feedback = require('./router/feedback')
+const home = require('./router/home')
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,14 +12,16 @@ app.use(express.json());
 //login route-------------------------
 app.use('/api/student', login)
 
-//show home page----------------------
-app.get('/home', (req, res) => {
-    res.send({ 'data': 'show home page' })
-})
+//show home page and search-----------
+app.use('/api', home)
 
 //reports route-----------------------
-app.use('/reports', report)
-    //mongoDB connection------------------
+app.use('/api/reports', report)
+
+//feedbacks route-----------------------
+app.use('/api/feedbacks', feedback)
+
+//mongoDB connection------------------
 mongoDB();
 
 //server start------------------------
