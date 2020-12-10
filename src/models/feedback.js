562570e2e73e0d8e79add6d39e7da94553ce6547
shrutiagrwal <shrutiagarwal518@gmail.com>
2020-12-10 21:13:28 +0530

    feedback model changed

--- src/models/feedback.js
+++ src/models/feedback.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const FeedbackSchema = new mongoose.Schema({
+    student: { type: mongoose.Schema.Types.ObjectId, ref: 'Students' },
     feedback: [{ type: String }],
     resolved: [{ type: Boolean, required: true, default: false }]
 })
