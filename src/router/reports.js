1d6a04d724f08676085d10f0ae1a832b0ad1c499
pawnikhanna <51890749+pawnikhanna@users.noreply.github.com>
2020-12-10 19:25:20 +0530

    Update reports.js

--- src/router/reports.js
+++ src/router/reports.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const router = express.Router();
-const Report = require('../models/reports')
 const auth = require('../auth/StudentAuth')
+const Company = require('../models/companies')
+const Report = require('../models/reports')
+const mongoose = require('mongoose')
 
-router.get('/reports', auth, async(req, res) => {
+//get all reports
+router.get('/',auth, async(req, res) => {
     try{
         const reports = await Report.find({});
-        res.send('reports');
+        if(!reports){
+            return res.status(404).send({ 'error': 'no data is available' });
+        }
+        else{
+            res.send(reports);
+        }
     }
     catch(err){
         res.status(400).send(err);
     }
 })
 
-router.get('/report/:id', async(req, res)=> {
+//get reports by id
+router.get('/:id',auth, async(req, res)=> {
     const report_id = req.params.id;
     try{
         const report = await Report.findById(report_id) ;
         if(!report){
-        return res.status(404).send();
+            return res.status(404).send({ 'error': 'no data is available' });
         }
         else{
             res.send(report);
@@ -29,4 +38,29 @@ router.get('/report/:id', async(req, res)=> {
     }
 });
 
-module.exports = router;
+//getting reports by companies
+router.get('/companies',auth , async(req, res) => {
+    try {
+        let reports = await mongoose.model("Reports").aggregate(
+            [{
+                    "$lookup": {
+                        from: "companies",
+                        localField: "companyId",
+                        foreignField: '_id',
+                        as: 'CompanyDetails'
+                    }
+                },
+                { "$group": { _id: "$CompanyDetails.name", reports: { $push: "$$ROOT" } } },
+                { "$sort": { _id: 1 } }
+            ]
+        )
+
+        res.send(reports)
+
+    } catch (err) {
+        console.log(err)
+        res.send({ 'error': 'no data is available' })
+    }
+})
+
+module.exports = router
