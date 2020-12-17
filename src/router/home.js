7b1df3f417619552ebba26d9606a4c6b77268c29
shrutiagrwal <shrutiagarwal518@gmail.com>
2020-12-17 20:32:06 +0530

    search route completed

--- src/router/home.js
+++ src/router/home.js
@@ -12,24 +12,21 @@ router.get('/home', (req, res) => {
 router.post('/search', auth, async(req, res) => {
     try {
         let name = req.body.company;
-        // let reg = new RegExp(/${name}/)
-        let companies = await Company.find({ Name: { $regex: `.*${name}.*`, $options: 'i' } })
-            // if (companies.length === 0)
-            //     return res.send({ data: 'No result available' })
-
-        // companies = await mongoose.model('Companies').aggregate(
-        //     [{
-        //             "$lookup": {
-        //                 from: "reports",
-        //                 localField: "reports",
-        //                 foreignField: "_id",
-        //                 as: "AllReports"
-        //             }
-        //         },
-        //         { "$project": { reports: 0 } }
-        //     ]
-        // )
+        companies = await mongoose.model('Companies').aggregate(
+            [{
+                    "$lookup": {
+                        from: "reports",
+                        localField: "reports",
+                        foreignField: "_id",
+                        as: "AllReports"
+                    }
+                },
+                { "$project": { reports: 0 } },
+                { $addFields: { results: { $regexMatch: { input: "$Name", regex: `.*${name}.*` } } } }
+            ]
+        )
         res.send(companies)
+
     } catch (err) {
         console.log(err)
         res.send({ 'error': 'no reports available for the company' })
